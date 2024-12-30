db3412753e0af1ebaeb936761c78c507e208ab6b
Adamy <admye@ucdavis.edu>
2024-12-30 01:17:56 -0500

    some progess with yolo, playing around with yolo11n interfaces

---
+++ train.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+
+
+# Train the model
+# results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+
+# Run inference on an image
+model.predict("test.jpg")
+
+
+
