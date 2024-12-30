df5adabcc85855c801f7b9479ff7a05cf3956682
Adamy <admye@ucdavis.edu>
2024-12-30 02:02:58 -0500

    ready to go()train(+)

--- TestwithCoindemo/train.py
+++ TestwithCoindemo/train.py
@@ -1,8 +1,13 @@
 from ultralytics import YOLO
 
+
+# Define the path to the dataset.yaml file
+dataset_yaml = "dataset.yaml"  # Path to your dataset.yaml file
+
+
 model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
 
 
 # Train the model
-results = model.train(data="dataset.yaml", epochs=1, imgsz=640)
+results = model.train(data="dataset.yaml", epochs=10, imgsz=640)
 
