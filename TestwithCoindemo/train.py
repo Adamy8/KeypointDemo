664ababef9e1e1698080feffb7a74c016b799fb2
ady <admye@ucdavis.edu>
2024-12-30 18:37:51 -0500

    updated windows paths for images/...

--- TestwithCoindemo/train.py
+++ TestwithCoindemo/train.py
@@ -6,16 +6,21 @@ dataset_yaml = "dataset.yaml"  # Path to your dataset.yaml file
 
 
 # model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
-model = YOLO("runs/detect/train3/weights/last.pt")
+model = YOLO("runs/detect/train9/weights/last.pt")
 
 
 # Train the model
 # results = model.train(data="dataset.yaml", epochs=10, imgsz=640)
 results = model.train(
     data="dataset.yaml", 
-    epochs=10, 
+    epochs=200,
     imgsz=640, 
-    save_dir="/Users/AdamYE_1/Desktop/KeypointDemo/TestwithCoindemo/runs/detect/train",
-    project="/Users/AdamYE_1/Desktop/KeypointDemo/TestwithCoindemo/runs/detect"  # Explicitly set project directory
+    # MAC
+    # save_dir="/Users/AdamYE_1/Desktop/KeypointDemo/TestwithCoindemo/runs/detect/train",
+    # project="/Users/AdamYE_1/Desktop/KeypointDemo/TestwithCoindemo/runs/detect"  # Explicitly set project directory
+
+    # Windows
+    save_dir="/home/ady/repos/KeypointDemo/TestwithCoindemo/runs/detect/train",
+    project="/home/ady/repos/KeypointDemo/TestwithCoindemo/runs/detect"  # Explicitly set project directory
 )
 
