43befa5e1709a85584e5269aa2430fbb1ecce3ca
ady <admye@ucdavis.edu>
2024-12-30 21:53:42 -0500

    ignore AwA-Pose, the annotation

--- train.py
+++ train.py
@@ -4,10 +4,8 @@ model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
 
 
 # Train the model
-# results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
-
-# Run inference on an image
-model.predict("test.jpg")
+results = model.train(data="config.yaml", epochs=100, imgsz=640)
 
 
-
+# # Run inference on an image
+# model.predict("test.jpg")
