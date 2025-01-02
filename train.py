4419f19618c19bcce094c50cb757ff1e52c14950
ady <admye@ucdavis.edu>
2025-01-02 14:23:42 -0500

    trained 525 now, 75 to go

--- train.py
+++ train.py
@@ -2,7 +2,14 @@ from ultralytics import YOLO
 
 # model = YOLO("yolov8n-pose.pt")
 
-model = YOLO("runs/pose/train2/weights/last.pt")
+model = YOLO("runs/pose/train4/weights/last.pt")
 
 
-results = model.train(data="config.yaml", epochs=470, imgsz=640)
+results = model.train(data="config.yaml", epochs=75, imgsz=640)
+
+
+# train:    5
+# train1:   25
+# train3:   470
+# train4:   25
+# train5:   75
