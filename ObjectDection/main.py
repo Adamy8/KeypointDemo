eb53866f1d8e2421a81509ab78e541f8d953999b
Adamy <admye@ucdavis.edu>
2025-04-15 18:28:30 -0700

    maintainence

--- ObjectDection/main.py
+++ ObjectDection/main.py
@@ -1,33 +1,25 @@
-from ultralytics import YOLO
-from ultralytics import solutions
 import cv2
 
-# model = YOLO("yolo11n.pt")
+from ultralytics import solutions
 
-distancecalculator = solutions.DistanceCalculation(
-    model="yolo11n.pt",
-    show=True,
-)
-
-w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
-video_writer = cv2.VideoWriter("distance_output.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
-
-# process video
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("test.mp4")
 assert cap.isOpened(), "Error reading video file"
+w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
+# Video writer
+video_writer = cv2.VideoWriter("distance_calculation.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
+
+# Init distance-calculation obj
+distance = solutions.DistanceCalculation(model="yolo11n.pt", show=True)
+
+# Process video
 while cap.isOpened():
     success, im0 = cap.read()
-
     if not success:
-        print("Video frame is empty or processing is complete.")
+        print("Video frame is empty or video processing has been successfully completed.")
         break
-
-    results = distancecalculator(im0)
-
-    print(results)  # access the output
-
-    video_writer.write(results.plot_im)  # write the processed frame.
+    im0 = distance.calculate(im0)
+    video_writer.write(im0)
 
 cap.release()
 video_writer.release()
